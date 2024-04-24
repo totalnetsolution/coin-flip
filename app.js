90661bbbdad61906f2c883c82549da413c9f83e9
muhammad-danish <totalnetsolution2@gmail.com>
2024-04-24 09:45:27 +0500

    index file created

--- app.js
+++ app.js
@@ -8,9 +8,26 @@ function flipCoin() {
     // Update coin image based on random number
     if (randomNumber === 0) {
         coinImg.src = "./image/head.jpg";
-        resultElement.textContent = "Heads!";
+        resultElement.textContent = "You Win";
     } else {
         coinImg.src = "./image/tails.jpg";
-        resultElement.textContent = "Tails!";
+        resultElement.textContent = "You Lose";
+    }
+}
+
+function tossCoin() {
+    const resultElement = document.getElementById('result');
+    const coinImg = document.getElementById('coin-img');
+
+    // Generate random number (0 or 1)
+    const randomNumber = Math.floor(Math.random() * 2);
+
+    // Update coin image based on random number
+    if (randomNumber === 1) {
+        coinImg.src = "./image/tails.jpg";
+        resultElement.textContent = "You Win";
+    } else {
+        coinImg.src = "./image/head.jpg";
+        resultElement.textContent = "You Lose";
     }
 }
